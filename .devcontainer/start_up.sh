0f6f3074dc0d6ef354a81aec75cd58450816bf9d
magic-sword <magic.sword.0001@gmail.com>
2024-12-05 03:20:38 +0900

    nvidiaのイメージに合わせて調整

--- .devcontainer/start_up.sh
+++ .devcontainer/start_up.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # jupyter notebookをバックアップで起動し、永続化 (ログイン不要)
-nohup $HOME/.local/bin/jupyter lab --ip='*' --NotebookApp.token='' --NotebookApp.password='' --config="/workspace/.devcontainer/jupyter_lab_config.py" &
+nohup jupyter lab --ip='*' --NotebookApp.token='' --NotebookApp.password='' --config="/workspace/.devcontainer/jupyter_lab_config.py" &
 
 # コンテナを永続化して、消さないようにする
 while sleep 1000; do :; done
